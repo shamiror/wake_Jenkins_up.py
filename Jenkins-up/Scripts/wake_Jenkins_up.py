e49f52693db71eafd1925090a90305b501aae58a
ors <ors@pixellot.tv>
2023-09-04 15:09:41 +0300

    Initial commit

---
+++ Jenkins-up/Scripts/wake_Jenkins_up.py
@@ -0,0 +1,32 @@
+
+import subprocess
+
+
+def run_batch_script():
+    return subprocess.Popen("runNewJenkins.bat", stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+
+def main():
+    process = run_batch_script()
+
+    while True:
+        line = process.stdout.readline().decode().strip()
+        if line:
+            print(line)
+            if "terminated" in line:
+                print("Detected 'terminated' in output. Restarting...")
+                process.terminate()
+                process.wait()
+                process = run_batch_script()
+
+        if process.poll() is not None:
+            print("Batch script terminated. Restarting...")
+            process = run_batch_script()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ Jenkins-up/runNewJenkins.bat
@@ -0,0 +1,1 @@
+java -jar agent.jar -jnlpUrl https://jenkins.aut.pixellot.tv/computer/pixBamboo/jenkins-agent.jnlp -secret bc7983eada5b2122a70dcbaa6130b541e8b1bab9162d682f2948af74016175bb -workDir "E:\Jenkins"
